5a6d6260509b335e3e1f44909d231a687cbb2ded
宗岩 <996290547@qq.com>
2017-09-11 13:54:03 +0800

    代码优化；

--- src/main/java/com/dzf/service/channel/impl/ChnPayBalanceServiceImpl.java
+++ src/main/java/com/dzf/service/channel/impl/ChnPayBalanceServiceImpl.java
@@ -1,7 +1,6 @@
 package com.dzf.service.channel.impl;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
