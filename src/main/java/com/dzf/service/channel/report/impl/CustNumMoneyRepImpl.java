75525b1fd582812158485aa8de26468f75736151
宗岩 <996290547@qq.com>
2017-11-15 17:35:39 +0800

    计算逻辑处理；

--- src/main/java/com/dzf/service/channel/report/impl/CustNumMoneyRepImpl.java
+++ src/main/java/com/dzf/service/channel/report/impl/CustNumMoneyRepImpl.java
@@ -248,7 +248,7 @@ public class CustNumMoneyRepImpl implements ICustNumMoneyRep {
 		sql.append("          FROM ynt_franchisee f \n") ; 
 		sql.append("         WHERE nvl(dr, 0) = 0 \n") ; 
 		sql.append("           AND nvl(f.isreport, 'N') = 'Y') \n");
-		sql.append("                   AND SUBSTR(t.dsigndate, 1, 7) > ? \n");
+		sql.append("                   AND SUBSTR(t.dsigndate, 1, 7) < ? \n");
 		spm.addParam(paramvo.getPeriod());
 		sql.append("                   AND nvl(t.icontracttype, 1) = 2\n");
 		sql.append("                   AND t.vdeductstatus = 2)) cu\n");//合同状态 = 已审核
