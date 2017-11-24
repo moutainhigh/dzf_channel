e193244d4474b9aa86f2816ba4eec25620862988
gejw <gejingwei@dazhangfang.com>
2017-11-24 17:55:16 +0800

    gjw
    Signed-off-by: gejw <gejingwei@dazhangfang.com>

--- src/main/java/com/dzf/service/channel/sys_power/impl/UserRoleServiceImpl.java
+++ src/main/java/com/dzf/service/channel/sys_power/impl/UserRoleServiceImpl.java
@@ -43,8 +43,8 @@ public class UserRoleServiceImpl implements IUserRoleService {
         sp.addParam(pk);
         StringBuffer sf = new StringBuffer();
         sf.append(" select sr.pk_role,sr.role_name,ur.pk_user_role from sm_role sr ");
-        sf.append(" left join sm_user_role ur on sr.pk_role = ur.pk_role and ur.cuserid = ?");
-        sf.append(" where sr.roletype = ? and sr.pk_corp = ?");
+        sf.append(" left join sm_user_role ur on sr.pk_role = ur.pk_role and ur.cuserid = ? and nvl(ur.dr,0) = 0");
+        sf.append(" where sr.roletype = ? and sr.pk_corp = ? ");
         sp.addParam(7);
         sp.addParam(pk_corp);
         List<URoleVO> list = (List<URoleVO>) singleObjectBO.executeQuery(sf.toString(), sp,
