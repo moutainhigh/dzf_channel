2ddd985a1a73aef145f4fcf516374da347a1f099
gejw <gejingwei@dazhangfang.com>
2017-11-24 15:47:14 +0800

    gjw
    Signed-off-by: gejw <gejingwei@dazhangfang.com>

--- src/main/java/com/dzf/action/sys/sys_power/ChnUserAction.java
+++ src/main/java/com/dzf/action/sys/sys_power/ChnUserAction.java
@@ -15,14 +15,10 @@ import com.dzf.model.pub.Json;
 import com.dzf.model.sys.sys_power.SysPowerConditVO;
 import com.dzf.model.sys.sys_power.UserVO;
 import com.dzf.pub.BusinessException;
-import com.dzf.pub.IDefaultValue;
 import com.dzf.pub.IGlobalConstants;
-import com.dzf.pub.ISysConstants;
 import com.dzf.pub.QueryDeCodeUtils;
 import com.dzf.pub.cache.UserCache;
 import com.dzf.pub.jm.CodeUtils1;
-import com.dzf.pub.lang.DZFBoolean;
-import com.dzf.service.pub.LogRecordEnum;
 import com.dzf.service.sys.sys_power.IUserService;
 
 /**
@@ -56,20 +52,16 @@ public class ChnUserAction extends BaseAction<UserVO>{
 		Json json = new Json();
 		if (data != null) {
 			try{
-				//FieldValidateUtils.Validate(data);
 				UserVO vo = ((UserVO)data);
 				String loginCorp = getLoginCorpInfo().getPk_corp();
-				
 				vo.setPk_creatcorp(loginCorp);//创建用户的公司
 				vo.setPk_corp(loginCorp);
 				if(vo.getPk_corp() == null)
 					throw new BusinessException("所属公司不能为空");
-				
 				data.setXsstyle(UTYPE);
 				UserVO[] datas = (UserVO[]) QueryDeCodeUtils.decKeyUtils(new String[]{"user_name","corpnm","crtcorp"}, new UserVO[]{data}, 0);
 				userService.save(datas[0]);
 				UserVO[] decdatas = (UserVO[]) QueryDeCodeUtils.decKeyUtils(new String[]{"user_name","corpnm","crtcorp"}, new UserVO[]{datas[0]}, 1);
-				
 				json.setSuccess(true);
 				json.setRows(decdatas[0]);
 				json.setMsg("保存成功");
@@ -83,13 +75,12 @@ public class ChnUserAction extends BaseAction<UserVO>{
 		writeJson(json);
 	}
 	
-	public void update(){//更新！
+	public void update(){
 		Json json = new Json();
 		if (data != null) {
 			try{
 				UserVO vo = ((UserVO)data);
 				String loginCorp = getLoginCorpInfo().getPk_corp();
-				
 				UserVO user = UserCache.getInstance().get(vo.getCuserid(), vo.getPk_corp());
 				if(user == null){
 					throw new BusinessException("该数据不存在或已删除！");
@@ -115,14 +106,13 @@ public class ChnUserAction extends BaseAction<UserVO>{
 	
 	public void query() throws Exception{
 		Grid grid = new Grid();
-		String loginCorp = IGlobalConstants.DefaultGroup;//(String) getSession().getAttribute(IGlobalConstants.login_corp);
+		String loginCorp = IGlobalConstants.DefaultGroup;
 		try {
 			SysPowerConditVO qryVO = new SysPowerConditVO();
 			String ilock = getRequest().getParameter("ilock");
 			qryVO.setCrtcorp_id(loginCorp);
 			qryVO.setUtype(UTYPE);
 			qryVO.setIlock(ilock);
-			//查询前对用户名加密 
 			if(qryVO !=null && qryVO.getUname() != null){
 				String uname = qryVO.getUname();
 				qryVO.setUname(CodeUtils1.enCode(uname));
@@ -163,51 +153,4 @@ public class ChnUserAction extends BaseAction<UserVO>{
 		writeJson(json);
 	}
 	
-
-
-	
-	public void updateUserVO(DZFBoolean islocked) throws Exception {
-		Json json = new Json();
-		if (data != null) {
-			try{
-				UserVO vo = (UserVO)data;
-				String loginCorp = getLoginCorpInfo().getPk_corp();
-				
-				UserVO user = UserCache.getInstance().get(vo.getCuserid(), vo.getPk_corp());
-				if(user == null){
-					throw new BusinessException("该数据不存在或已删除！");
-				}
-				
-				vo.setLocked_tag(islocked);
-				userService.updateLock(vo);
-				json.setSuccess(true);
-				json.setRows(data);
-				if(islocked.booleanValue()){
-					json.setMsg("锁定成功");
-					if(loginCorp.equals(IDefaultValue.DefaultGroup)){
-						writeLogRecord(LogRecordEnum.OPE_JITUAN_QXGL.getValue(),"锁定用户:用户编码"+user.getUser_code(),ISysConstants.SYS_0);
-					}else{
-						writeLogRecord(LogRecordEnum.OPE_ADMIN_YHGL.getValue(),"锁定用户:用户编码"+user.getUser_code(),ISysConstants.SYS_1);
-					}
-				}else{
-					json.setMsg("解锁成功");
-					if(loginCorp.equals(IDefaultValue.DefaultGroup)){
-						writeLogRecord(LogRecordEnum.OPE_JITUAN_QXGL.getValue(),"解锁用户:用户编码"+user.getUser_code(),ISysConstants.SYS_0);
-					}else{
-						writeLogRecord(LogRecordEnum.OPE_ADMIN_YHGL.getValue(),"解锁用户:用户编码"+user.getUser_code(),ISysConstants.SYS_1);
-					}
-				}
-			}catch(Exception e){
-				printErrorLog(json, log, e, "操作失败");
-			}
-		}else{
-			json.setSuccess(false);
-			if(islocked.booleanValue()){
-				json.setMsg("锁定失败:数据为空。");
-			}else{
-				json.setMsg("解锁失败:数据为空。");
-			}
-		}
-		writeJson(json);
-	}
 }
