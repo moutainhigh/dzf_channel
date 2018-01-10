795dcb26831c2c3f2e2b7d10314652a35cb0c21e
宗岩 <996290547@qq.com>
2018-01-10 14:51:35 +0800

    销售数据分析问题处理；

--- src/main/java/com/dzf/service/channel/chn_set/impl/SaleAnalyseServiceImpl.java
+++ src/main/java/com/dzf/service/channel/chn_set/impl/SaleAnalyseServiceImpl.java
@@ -237,7 +237,7 @@ public class SaleAnalyseServiceImpl implements ISaleAnalyseService {
 		sql.append("   AND nvl(s.ibusitype, 0) = 1 \n") ; 
 		sql.append("   AND s.irecestatus IN (2, 4)\n") ; 
 		sql.append("   AND t.icosttype IN (0, 1)\n") ; 
-		sql.append("   AND ( (t.vstatus = 1 AND nvl(t.icontracttype,0) = 1 ) OR ( vdeductstatus = 1 AND nvl(t.icontracttype,0) = 2))\n") ; 
+		sql.append("   AND ( (t.vstatus = 1 AND nvl(t.icontracttype,1) = 1 ) OR ( t.vdeductstatus = 1 AND nvl(t.icontracttype,1) = 2))\n") ; 
 		if(qvo.getDbegindate() != null){
 			sql.append("   AND t.dbegindate >= ? \n");
 			spm.addParam(qvo.getDbegindate());
