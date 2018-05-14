7440c9f21e564753aab2a308e5a263ef71155097
宗岩 <996290547@qq.com>
2018-05-14 11:00:22 +0800

    测试

--- Embedded/webapps/dzf_channel/js/channel/chn_set/rejectreason.js
+++ Embedded/webapps/dzf_channel/js/channel/chn_set/rejectreason.js
@@ -55,7 +55,6 @@ function load(){
  * @returns {String}
  */
 function opermatter(val, row, index) {
-	return'';
 //	return '<a href="javascript:void(0)" class="ui-btn ui-btn-xz" style="margin-bottom:0px;" onclick="onEdit(' + index + ')">修改</a>'+
 //	'<a href="javascript:void(0)" class="ui-btn ui-btn-xz" style="margin-bottom:0px;" onclick="onDelete(this)">删除</a>';
 	return '<a href="javascript:void(0)" class="ui-btn ui-btn-xz" style="margin-bottom:0px;" onclick="onEdit(' + index + ')">修改</a>'
