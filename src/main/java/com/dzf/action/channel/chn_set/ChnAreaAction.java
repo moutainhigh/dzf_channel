8655447455eb39a31629908a045a4f87b14c7434
宗岩 <996290547@qq.com>
2018-07-13 11:23:03 +0800

    添加功能节点权限校验；

--- src/main/java/com/dzf/action/channel/chn_set/ChnAreaAction.java
+++ src/main/java/com/dzf/action/channel/chn_set/ChnAreaAction.java
@@ -1,4 +1,5 @@
 package com.dzf.action.channel.chn_set;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
@@ -22,14 +23,17 @@ import com.dzf.model.pub.ComboBoxVO;
 import com.dzf.model.pub.Grid;
 import com.dzf.model.pub.Json;
 import com.dzf.model.pub.QryParamVO;
+import com.dzf.model.sys.sys_power.UserVO;
 import com.dzf.pub.BusinessException;
 import com.dzf.pub.DzfTypeUtils;
 import com.dzf.pub.StringUtil;
 import com.dzf.pub.Field.FieldMapping;
+import com.dzf.pub.constant.IFunNode;
 import com.dzf.pub.lang.DZFDate;
 import com.dzf.pub.lang.DZFDateTime;
 import com.dzf.pub.util.JSONConvtoJAVA;
 import com.dzf.service.channel.chn_set.IChnAreaService;
+import com.dzf.service.pub.IPubService;
 
 /**
  * 渠道区域划分或培训区域划分
@@ -41,19 +45,29 @@ import com.dzf.service.channel.chn_set.IChnAreaService;
 @Action(value = "chnarea")
 public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 
-	@Autowired
-	private IChnAreaService chnarea;
-
 	private Logger log = Logger.getLogger(this.getClass());
 	
+	@Autowired
+	private IChnAreaService chnarea;
+	
+	@Autowired
+	private IPubService pubser;
+
 	/**
 	 * 保存主表数据
 	 */
 	public void save() {
 		Json json = new Json();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
+			pubser.checkFunnode(uservo, IFunNode.CHANNEL_15);
 			ChnAreaVO headvo = requestDealStep(getRequest());
-			headvo=chnarea.save(headvo);
+			headvo = chnarea.save(headvo);
 			json.setSuccess(true);
 			json.setRows(headvo);
 			json.setMsg("保存成功!");
@@ -62,67 +76,66 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		}
 		writeJson(json);
 	}
-	
-	
+
 	private ChnAreaVO requestDealStep(HttpServletRequest request) {
 		String head = request.getParameter("head");
-		String body = request.getParameter("body"); //子表
+		String body = request.getParameter("body"); // 子表
 		body = body.replace("}{", "},{");
 		body = "[" + body + "]";
 		JSON headjs = (JSON) JSON.parse(head);
 		JSONArray array = (JSONArray) JSON.parseArray(body);
 		Map<String, String> headmaping = FieldMapping.getFieldMapping(new ChnAreaVO());
 		Map<String, String> bodymapping = FieldMapping.getFieldMapping(new ChnAreaBVO());
-		
+
 		ChnAreaVO headvo = DzfTypeUtils.cast(headjs, headmaping, ChnAreaVO.class, JSONConvtoJAVA.getParserConfig());
 		String pk_corp = headvo.getPk_corp();
 		if (StringUtil.isEmpty(pk_corp)) {
 			pk_corp = getLoginCorpInfo().getPk_corp();
 			headvo.setPk_corp(pk_corp);
 		}
-		if(StringUtil.isEmpty(headvo.getPk_chnarea())){
+		if (StringUtil.isEmpty(headvo.getPk_chnarea())) {
 			headvo.setDoperatedate(new DZFDate());
 			headvo.setCoperatorid(getLoginUserInfo().getCuserid());
 			headvo.setTs(new DZFDateTime());
 		}
-		ChnAreaBVO[] bodyvos= DzfTypeUtils.cast(array, bodymapping, ChnAreaBVO[].class,
+		ChnAreaBVO[] bodyvos = DzfTypeUtils.cast(array, bodymapping, ChnAreaBVO[].class,
 				JSONConvtoJAVA.getParserConfig());
-		HashMap<String, String> map1=new HashMap<>();
-		HashMap<String, String> map2=new HashMap<>();
+		HashMap<String, String> map1 = new HashMap<>();
+		HashMap<String, String> map2 = new HashMap<>();
 		List<ChnAreaBVO> list = new ArrayList<ChnAreaBVO>();
 		for (ChnAreaBVO chnAreaBVO : bodyvos) {
-			if(!chnAreaBVO.getIsCharge().booleanValue() && StringUtil.isEmpty(chnAreaBVO.getPk_corp())){
+			if (!chnAreaBVO.getIsCharge().booleanValue() && StringUtil.isEmpty(chnAreaBVO.getPk_corp())) {
 				throw new BusinessException("省市负责人为否时，加盟商为必输项");
 			}
-			if(!StringUtil.isEmpty(chnAreaBVO.getUserid())){
-				String id1=chnAreaBVO.getVprovince()+chnAreaBVO.getUserid();
-				if(map1.containsKey(id1)){
-					if(headvo.getType()==1){
+			if (!StringUtil.isEmpty(chnAreaBVO.getUserid())) {
+				String id1 = chnAreaBVO.getVprovince() + chnAreaBVO.getUserid();
+				if (map1.containsKey(id1)) {
+					if (headvo.getType() == 1) {
 						throw new BusinessException("负责地区+渠道经理重复,请重新输入");
-					}else if(headvo.getType()==2){
+					} else if (headvo.getType() == 2) {
 						throw new BusinessException("负责地区+培训师重复,请重新输入");
-					}else{
+					} else {
 						throw new BusinessException("负责地区+渠道运营重复,请重新输入");
 					}
-				}else{
-					map1.put(id1,"value");
+				} else {
+					map1.put(id1, "value");
 				}
 			}
-			if(chnAreaBVO.getIsCharge().booleanValue()){
-				String id2=chnAreaBVO.getVprovince()+chnAreaBVO.getIsCharge().toString();
-				if(map2.containsKey(id2)){
+			if (chnAreaBVO.getIsCharge().booleanValue()) {
+				String id2 = chnAreaBVO.getVprovince() + chnAreaBVO.getIsCharge().toString();
+				if (map2.containsKey(id2)) {
 					throw new BusinessException("一个地区只能有一个负责人");
-				}else{
-					map2.put(id2,"value");
+				} else {
+					map2.put(id2, "value");
 				}
 			}
 			chnAreaBVO.setType(headvo.getType());
-			if(StringUtil.isEmpty(chnAreaBVO.getPk_corp()) || !chnAreaBVO.getPk_corp().contains(",")){
+			if (StringUtil.isEmpty(chnAreaBVO.getPk_corp()) || !chnAreaBVO.getPk_corp().contains(",")) {
 				list.add(chnAreaBVO);
-			}else{
+			} else {
 				String[] corps = chnAreaBVO.getPk_corp().split(",");
 				for (String corp : corps) {
-					ChnAreaBVO clone = (ChnAreaBVO)chnAreaBVO.clone();
+					ChnAreaBVO clone = (ChnAreaBVO) chnAreaBVO.clone();
 					clone.setPk_corp(corp);
 					list.add(clone);
 				}
@@ -138,12 +151,19 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 	public void delete() {
 		Json json = new Json();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
+			pubser.checkFunnode(uservo, IFunNode.CHANNEL_15);
 			String pk_area = getRequest().getParameter("pk_area");
-			if(StringUtil.isEmpty(pk_area)){
+			if (StringUtil.isEmpty(pk_area)) {
 				throw new BusinessException("主键为空");
 			}
 			String pk_corp = getLoginCorpInfo().getPk_corp();
-			chnarea.delete(pk_area,pk_corp);
+			chnarea.delete(pk_area, pk_corp);
 			json.setSuccess(true);
 			json.setRows(data);
 			json.setMsg("删除成功!");
@@ -153,13 +173,18 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		writeJson(json);
 	}
 
-
 	/**
 	 * 查询主表数据
 	 */
 	public void query() {
 		Grid grid = new Grid();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
 			ChnAreaVO qvo = new ChnAreaVO();
 			qvo = (ChnAreaVO) DzfTypeUtils.cast(getRequest(), qvo);
 			String pk_corp = getLogincorppk();
@@ -167,10 +192,10 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 				qvo.setPk_corp(pk_corp);
 			}
 			ChnAreaVO[] vos = chnarea.query(qvo);
-			if(vos==null||vos.length==0){
+			if (vos == null || vos.length == 0) {
 				grid.setRows(new ArrayList<ChnAreaVO>());
 				grid.setMsg("查询数据为空!");
-			}else{
+			} else {
 				grid.setRows(Arrays.asList(vos));
 				grid.setSuccess(true);
 				grid.setMsg("查询成功!");
@@ -184,11 +209,17 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 	/**
 	 * 通过主表主键查询主子表数据
 	 */
-	public void queryByPrimaryKey(){
+	public void queryByPrimaryKey() {
 		Grid grid = new Grid();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
 			String pk_area = getRequest().getParameter("pk_area");
-			if(StringUtil.isEmpty(pk_area)){
+			if (StringUtil.isEmpty(pk_area)) {
 				throw new BusinessException("主键为空");
 			}
 			ChnAreaVO vo = chnarea.queryByPrimaryKey(pk_area);
@@ -202,15 +233,21 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		}
 		writeJson(grid);
 	}
-	
+
 	/**
 	 * 查询加盟商总经理
 	 */
-	public void queryManager(){
+	public void queryManager() {
 		Json json = new Json();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
 			String pk_corp = getLoginCorpInfo().getPk_corp();
-			String username= chnarea.queryManager(pk_corp);
+			String username = chnarea.queryManager(pk_corp);
 			json.setRows(username);
 			json.setSuccess(true);
 			json.setMsg("操作成功");
@@ -220,20 +257,26 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		}
 		writeJson(json);
 	}
-	
+
 	/**
 	 * 查询省市，过滤掉那些已经使用的
 	 */
-	public void queryComboxArea(){
+	public void queryComboxArea() {
 		Grid grid = new Grid();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
 			String pk_area = getRequest().getParameter("pk_area");
 			String type = getRequest().getParameter("type");
-			ArrayList list = chnarea.queryComboxArea(pk_area,type);
-			if(list==null||list.size()==0){
+			ArrayList list = chnarea.queryComboxArea(pk_area, type);
+			if (list == null || list.size() == 0) {
 				grid.setRows(null);
 				grid.setMsg("查询数据为空!");
-			}else{
+			} else {
 				grid.setRows(list);
 				grid.setSuccess(true);
 				grid.setMsg("查询成功!");
@@ -243,22 +286,28 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		}
 		writeJson(grid);
 	}
-	
+
 	/**
 	 * 查询大区（下拉选项）
 	 */
 	public void queryArea() {
 		Grid grid = new Grid();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
 			QryParamVO qvo = new QryParamVO();
 			qvo = (QryParamVO) DzfTypeUtils.cast(getRequest(), qvo);
 			qvo.setCuserid(getLoginUserid());
 			List<ComboBoxVO> vos = chnarea.queryArea(qvo);
-			if(vos==null||vos.size()==0){
+			if (vos == null || vos.size() == 0) {
 				grid.setRows(new ArrayList<ChnAreaVO>());
 				grid.setSuccess(true);
 				grid.setMsg("查询数据为空!");
-			}else{
+			} else {
 				grid.setRows(vos);
 				grid.setSuccess(true);
 				grid.setMsg("查询成功!");
@@ -268,22 +317,28 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		}
 		writeJson(grid);
 	}
-	
+
 	/**
 	 * 查询省（市）（下拉选项）
 	 */
 	public void queryProvince() {
 		Grid grid = new Grid();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
 			QryParamVO qvo = new QryParamVO();
 			qvo = (QryParamVO) DzfTypeUtils.cast(getRequest(), qvo);
 			qvo.setCuserid(getLoginUserid());
 			List<ComboBoxVO> vos = chnarea.queryProvince(qvo);
-			if(vos==null||vos.size()==0){
+			if (vos == null || vos.size() == 0) {
 				grid.setRows(new ArrayList<ChnAreaVO>());
 				grid.setSuccess(true);
 				grid.setMsg("查询数据为空!");
-			}else{
+			} else {
 				grid.setRows(vos);
 				grid.setSuccess(true);
 				grid.setMsg("查询成功!");
@@ -293,23 +348,29 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		}
 		writeJson(grid);
 	}
-	
+
 	/**
 	 * 查询省级别 人（下拉选项）
-	 * 				   
+	 * 
 	 */
 	public void queryTrainer() {
 		Grid grid = new Grid();
 		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
 			QryParamVO qvo = new QryParamVO();
 			qvo = (QryParamVO) DzfTypeUtils.cast(getRequest(), qvo);
 			qvo.setCuserid(getLoginUserid());
 			List<ComboBoxVO> vos = chnarea.queryTrainer(qvo);
-			if(vos==null||vos.size()==0){
+			if (vos == null || vos.size() == 0) {
 				grid.setRows(new ArrayList<ChnAreaVO>());
 				grid.setSuccess(true);
 				grid.setMsg("查询数据为空!");
-			}else{
+			} else {
 				grid.setRows(vos);
 				grid.setSuccess(true);
 				grid.setMsg("查询成功!");
@@ -319,5 +380,5 @@ public class ChnAreaAction extends BaseAction<ChnAreaVO> {
 		}
 		writeJson(grid);
 	}
-	
+
 }
