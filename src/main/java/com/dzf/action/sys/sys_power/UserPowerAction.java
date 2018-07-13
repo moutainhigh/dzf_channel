94383946416bf57131011062a19aa5b816c9656a
宗岩 <996290547@qq.com>
2018-07-13 11:13:15 +0800

    添加功能节点权限校验；

--- src/main/java/com/dzf/action/sys/sys_power/UserPowerAction.java
+++ src/main/java/com/dzf/action/sys/sys_power/UserPowerAction.java
@@ -13,10 +13,13 @@ import com.dzf.action.pub.BaseAction;
 import com.dzf.model.channel.sys_power.URoleVO;
 import com.dzf.model.pub.Grid;
 import com.dzf.model.sys.sys_power.UserVO;
+import com.dzf.pub.BusinessException;
 import com.dzf.pub.ISysConstants;
 import com.dzf.pub.StringUtil;
 import com.dzf.pub.cache.UserCache;
+import com.dzf.pub.constant.IFunNode;
 import com.dzf.service.channel.sys_power.IUserRoleService;
+import com.dzf.service.pub.IPubService;
 import com.dzf.service.pub.LogRecordEnum;
 
 @ParentPackage("basePackage")
@@ -24,79 +27,98 @@ import com.dzf.service.pub.LogRecordEnum;
 @Action(value = "userPower")
 public class UserPowerAction extends BaseAction<URoleVO> {
 
-    private Logger log = Logger.getLogger(this.getClass());
-    @Autowired
-    private IUserRoleService userRoleServiceImpl;
+	private static final long serialVersionUID = -3194454292538205899L;
 
-    /**
-     * 
-     */
-    public void queryUserRole() {
-        Grid grid = new Grid();
-        try {
-            String pk_corp = getLogincorppk();
-            String pk = null;
-            if (data != null) {
-                pk = data.getCuserid();
-            }
-            List<URoleVO> list = userRoleServiceImpl.queryUserRoleVO(pk, pk_corp);
-            if (list != null && list.size() > 0) {
-                writeJson(list);
-                return;
-            } else {
-                grid.setTotal(0L);
-                grid.setSuccess(false);
-                grid.setMsg("查询失败!");
-                grid.setRows(null);
-            }
-        } catch (Exception e) {
-            printErrorLog(grid, log, e, "查询失败!");
-        }
-        writeJson(grid);
-    }
+	private Logger log = Logger.getLogger(this.getClass());
+	
+	@Autowired
+	private IUserRoleService userRoleServiceImpl;
+	
+	@Autowired
+	private IPubService pubser;
 
-    public void saveUserRoleVO() {
-        Grid grid = new Grid();
-        grid.setSuccess(false);
-        grid.setMsg("数据为空，保存失败！");
-        try {
-            String pk_corp = getLogincorppk();
-            URoleVO[] vos = null;
-            if (data != null) {
-                String userid = data.getCuserid();
-                String[] roleids = data.getCkaccarr();
-                if (!StringUtil.isEmpty(userid)) {
-                    vos = buildRoleVOs(roleids, userid);
-                    userRoleServiceImpl.saveUserRoleVO(vos, userid, pk_corp);
-                    grid.setSuccess(true);
-                    grid.setMsg("保存成功！");
-                    UserVO uvo = UserCache.getInstance().get(userid, null);
-                    StringBuffer msg = new StringBuffer("分配权限");
-                    if(uvo != null){
-                        msg.append(":").append(uvo.getUser_code()).append(" ").append(uvo.getUser_name()) ;
-                    }
-                    writeLogRecord(LogRecordEnum.OPE_CHANNEL_QXFP.getValue(),msg.toString() ,ISysConstants.SYS_3);
-                }
-            }
-        } catch (Exception e) {
-            printErrorLog(grid, log, e, "保存失败!");
-        }
-        writeJson(grid);
-    }
+	/**
+	 * 
+	 */
+	public void queryUserRole() {
+		Grid grid = new Grid();
+		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
+			String pk_corp = getLogincorppk();
+			String pk = null;
+			if (data != null) {
+				pk = data.getCuserid();
+			}
+			List<URoleVO> list = userRoleServiceImpl.queryUserRoleVO(pk, pk_corp);
+			if (list != null && list.size() > 0) {
+				writeJson(list);
+				return;
+			} else {
+				grid.setTotal(0L);
+				grid.setSuccess(false);
+				grid.setMsg("查询失败!");
+				grid.setRows(null);
+			}
+		} catch (Exception e) {
+			printErrorLog(grid, log, e, "查询失败!");
+		}
+		writeJson(grid);
+	}
 
-    private URoleVO[] buildRoleVOs(String[] roleids, String cuserid) {
-        if (StringUtil.isEmpty(cuserid) || roleids == null || roleids.length == 0)
-            return null;
-        List<URoleVO> va = new ArrayList<URoleVO>();
-        for (String roleid : roleids) {
-            if (!StringUtil.isEmpty(roleid) && roleid.length() == 24) {
-                URoleVO vo = new URoleVO();
-                vo.setPk_corp(getLogincorppk());
-                vo.setPk_role(roleid);
-                vo.setCuserid(cuserid);
-                va.add(vo);
-            }
-        }
-        return va.toArray(new URoleVO[0]);
-    }
+	public void saveUserRoleVO() {
+		Grid grid = new Grid();
+		grid.setSuccess(false);
+		grid.setMsg("数据为空，保存失败！");
+		try {
+			UserVO uservo = getLoginUserInfo();
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
+				throw new BusinessException("登陆用户错误");
+			} else if (uservo == null) {
+				throw new BusinessException("登陆用户错误");
+			}
+			pubser.checkFunnode(uservo, IFunNode.CHANNEL_21);
+			String pk_corp = getLogincorppk();
+			URoleVO[] vos = null;
+			if (data != null) {
+				String userid = data.getCuserid();
+				String[] roleids = data.getCkaccarr();
+				if (!StringUtil.isEmpty(userid)) {
+					vos = buildRoleVOs(roleids, userid);
+					userRoleServiceImpl.saveUserRoleVO(vos, userid, pk_corp);
+					grid.setSuccess(true);
+					grid.setMsg("保存成功！");
+					UserVO uvo = UserCache.getInstance().get(userid, null);
+					StringBuffer msg = new StringBuffer("分配权限");
+					if (uvo != null) {
+						msg.append(":").append(uvo.getUser_code()).append(" ").append(uvo.getUser_name());
+					}
+					writeLogRecord(LogRecordEnum.OPE_CHANNEL_QXFP.getValue(), msg.toString(), ISysConstants.SYS_3);
+				}
+			}
+		} catch (Exception e) {
+			printErrorLog(grid, log, e, "保存失败!");
+		}
+		writeJson(grid);
+	}
+
+	private URoleVO[] buildRoleVOs(String[] roleids, String cuserid) {
+		if (StringUtil.isEmpty(cuserid) || roleids == null || roleids.length == 0)
+			return null;
+		List<URoleVO> va = new ArrayList<URoleVO>();
+		for (String roleid : roleids) {
+			if (!StringUtil.isEmpty(roleid) && roleid.length() == 24) {
+				URoleVO vo = new URoleVO();
+				vo.setPk_corp(getLogincorppk());
+				vo.setPk_role(roleid);
+				vo.setCuserid(cuserid);
+				va.add(vo);
+			}
+		}
+		return va.toArray(new URoleVO[0]);
+	}
 }
