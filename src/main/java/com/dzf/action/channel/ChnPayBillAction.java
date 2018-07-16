f6bb4dcd691994556649e5a2b7dee33877566911
宗岩 <996290547@qq.com>
2018-07-16 14:02:44 +0800

    添加操作日志；

--- src/main/java/com/dzf/action/channel/ChnPayBillAction.java
+++ src/main/java/com/dzf/action/channel/ChnPayBillAction.java
@@ -28,6 +28,7 @@ import com.dzf.model.sys.sys_power.CorpVO;
 import com.dzf.model.sys.sys_power.UserVO;
 import com.dzf.pub.BusinessException;
 import com.dzf.pub.DzfTypeUtils;
+import com.dzf.pub.ISysConstants;
 import com.dzf.pub.StringUtil;
 import com.dzf.pub.Field.FieldMapping;
 import com.dzf.pub.constant.IFunNode;
@@ -36,6 +37,7 @@ import com.dzf.pub.util.JSONConvtoJAVA;
 import com.dzf.pub.util.QueryUtil;
 import com.dzf.service.channel.IChnPayService;
 import com.dzf.service.pub.IPubService;
+import com.dzf.service.pub.LogRecordEnum;
 
 /**
  * 渠道-付款单
@@ -47,33 +49,32 @@ import com.dzf.service.pub.IPubService;
 public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 
 	private static final long serialVersionUID = -5179718423895024141L;
-	
+
 	private Logger log = Logger.getLogger(this.getClass());
-	
+
 	@Autowired
 	private IChnPayService chnpay;
-	
+
 	@Autowired
 	private IPubService pubser;
-
 
 	public void query() {
 		Grid grid = new Grid();
 		try {
 			UserVO uservo = getLoginUserInfo();
-			if(uservo != null && !"000001".equals(uservo.getPk_corp()) ){
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
 				throw new BusinessException("登陆用户错误");
-			}else if(uservo == null){
+			} else if (uservo == null) {
 				throw new BusinessException("登陆用户错误");
 			}
 			ChnPayBillVO paramvo = new ChnPayBillVO();
-		    paramvo = (ChnPayBillVO) DzfTypeUtils.cast(getRequest(), paramvo);
-			int page = paramvo == null ?1: paramvo.getPage();
-			int rows = paramvo ==null? 100000: paramvo.getRows();
-			ChnPayBillVO[]  vos = chnpay.query(paramvo, getLoginUserInfo());
-			int len = vos==null?0:vos.length;
+			paramvo = (ChnPayBillVO) DzfTypeUtils.cast(getRequest(), paramvo);
+			int page = paramvo == null ? 1 : paramvo.getPage();
+			int rows = paramvo == null ? 100000 : paramvo.getRows();
+			ChnPayBillVO[] vos = chnpay.query(paramvo, getLoginUserInfo());
+			int len = vos == null ? 0 : vos.length;
 			if (vos != null && vos.length > 0) {
-				vos = (ChnPayBillVO[]) QueryUtil.getPagedVOs(vos,page,rows);
+				vos = (ChnPayBillVO[]) QueryUtil.getPagedVOs(vos, page, rows);
 				grid.setRows(Arrays.asList(vos));
 				grid.setTotal((long) (len));
 				grid.setMsg("查询成功!");
@@ -88,7 +89,6 @@ public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 		writeJson(grid);
 	}
 
-
 	/**
 	 * 保存
 	 */
@@ -98,33 +98,46 @@ public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 			try {
 				UserVO uservo = getLoginUserInfo();
 				pubser.checkFunnode(uservo, IFunNode.CHANNEL_36);
-				if(uservo != null && !"000001".equals(uservo.getPk_corp()) ){
+				if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
 					throw new BusinessException("登陆用户错误");
-				}else if(uservo == null){
+				} else if (uservo == null) {
 					throw new BusinessException("登陆用户错误");
+				}
+				String opertype = "";
+				if(StringUtil.isEmpty(data.getPk_paybill())){
+					opertype = "isAdd";
+				}else{
+					opertype = "isEdit";
 				}
 				CorpVO accvo = getLoginCorpInfo();
 				File[] files = ((MultiPartRequestWrapper) getRequest()).getFiles("pFile");
 				String[] filenames = ((MultiPartRequestWrapper) getRequest()).getFileNames("pFile");
-				data=setDefault(data);
-	 			data= chnpay.save(data,accvo,getLoginUserInfo().getCuserid(),files,filenames);
+				data = setDefault(data);
+				data = chnpay.save(data, accvo, getLoginUserInfo().getCuserid(), files, filenames);
+				if(data != null){
+					if("isAdd".equals(opertype)){
+						writeLogRecord(LogRecordEnum.OPE_CHANNEL_36.getValue(), "新增付款单：单据号："+data.getVbillcode(), ISysConstants.SYS_3);
+					}else if("isEdit".equals(opertype)){
+						writeLogRecord(LogRecordEnum.OPE_CHANNEL_36.getValue(), "修改付款单：单据号："+data.getVbillcode(), ISysConstants.SYS_3);
+					}
+				}
 				json.setRows(Arrays.asList(data));
-				json.setSuccess(true);	
+				json.setSuccess(true);
 				json.setMsg("保存成功!");
 			} catch (Exception e) {
 				printErrorLog(json, log, e, "保存失败");
 			}
-		}else {
+		} else {
 			json.setSuccess(false);
 			json.setMsg("保存失败");
 		}
 		writeJson(json);
 	}
-	
-	//新增，默认录入人与日期
-	private ChnPayBillVO setDefault(ChnPayBillVO chn){
-		if(StringUtil.isEmpty(chn.getPk_paybill())){
-			if(chn.getDoperatedate()==null){
+
+	// 新增，默认录入人与日期
+	private ChnPayBillVO setDefault(ChnPayBillVO chn) {
+		if (StringUtil.isEmpty(chn.getPk_paybill())) {
+			if (chn.getDoperatedate() == null) {
 				chn.setDoperatedate(new DZFDate());
 			}
 			chn.setCoperatorid(getLoginUserInfo().getCuserid());
@@ -132,15 +145,15 @@ public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 		}
 		return chn;
 	}
-	
+
 	public void updateStatus() {
 		Json json = new Json();
 		try {
 			UserVO uservo = getLoginUserInfo();
 			pubser.checkFunnode(uservo, IFunNode.CHANNEL_36);
-			if(uservo != null && !"000001".equals(uservo.getPk_corp()) ){
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
 				throw new BusinessException("登陆用户错误");
-			}else if(uservo == null){
+			} else if (uservo == null) {
 				throw new BusinessException("登陆用户错误");
 			}
 			String chns = getRequest().getParameter("chns");
@@ -149,20 +162,19 @@ public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 			chns = "[" + chns + "]";
 			JSONArray array = (JSONArray) JSON.parseArray(chns);
 			Map<String, String> map = FieldMapping.getFieldMapping(new ChnPayBillVO());
-			ChnPayBillVO[] cons = DzfTypeUtils.cast(array,  map, ChnPayBillVO[].class,
-					JSONConvtoJAVA.getParserConfig());
+			ChnPayBillVO[] cons = DzfTypeUtils.cast(array, map, ChnPayBillVO[].class, JSONConvtoJAVA.getParserConfig());
 			if (cons.length == 0 || StringUtil.isEmpty(temp)) {
 				throw new BusinessException("参数错误");
 			}
 			int stat = Integer.parseInt(temp);
-			if(stat==2){
-				cons[0].setSubmitid(getLoginUserid());//提单人
+			if (stat == 2) {
+				cons[0].setSubmitid(getLoginUserid());// 提单人
 			}
 			HashMap<String, Object> cmap = chnpay.updateStatusMult(cons, stat);
-			if(cmap.get("stat").equals("1")){
+			if (cmap.get("stat").equals("1")) {
 				json.setStatus(-1);
 			}
-			String errmsg = (String)cmap.get("errmsg");
+			String errmsg = (String) cmap.get("errmsg");
 			json.setSuccess(true);
 			json.setMsg(errmsg);
 		} catch (Exception e) {
@@ -174,21 +186,21 @@ public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 	/**
 	 * 查询单个数据
 	 */
-	public void queryByID(){
+	public void queryByID() {
 		Json json = new Json();
 		try {
 			UserVO uservo = getLoginUserInfo();
-			if(uservo != null && !"000001".equals(uservo.getPk_corp()) ){
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
 				throw new BusinessException("登陆用户错误");
-			}else if(uservo == null){
+			} else if (uservo == null) {
 				throw new BusinessException("登陆用户错误");
 			}
 			ChnPayBillVO chn = new ChnPayBillVO();
 			chn = (ChnPayBillVO) DzfTypeUtils.cast(getRequest(), chn);
-			if(StringUtil.isEmpty(chn.getPk_paybill())){
+			if (StringUtil.isEmpty(chn.getPk_paybill())) {
 				throw new BusinessException("主键为空");
 			}
-			if (StringUtil.isEmpty(chn.getPk_corp())){
+			if (StringUtil.isEmpty(chn.getPk_corp())) {
 				chn.setPk_corp(getLogincorppk());
 			}
 			ChnPayBillVO recust = chnpay.queryByID(chn.getPk_paybill());
@@ -221,72 +233,71 @@ public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 		}
 		writeJson(json);
 	}
-	
-	
+
 	/****
 	 * 获取附件显示图片
 	 */
-	public void getAttachImage(){
+	public void getAttachImage() {
 		InputStream is = null;
 		OutputStream os = null;
 		try {
-			ChnPayBillVO vo=new ChnPayBillVO();
+			ChnPayBillVO vo = new ChnPayBillVO();
 			vo = (ChnPayBillVO) DzfTypeUtils.cast(getRequest(), vo);
-			if(StringUtil.isEmpty(vo.getPk_paybill())){
+			if (StringUtil.isEmpty(vo.getPk_paybill())) {
 				throw new BusinessException("主键为空");
 			}
-			if (StringUtil.isEmpty(vo.getPk_corp())){
+			if (StringUtil.isEmpty(vo.getPk_corp())) {
 				vo.setPk_corp(getLogincorppk());
 			}
 			vo = chnpay.queryByID(vo.getPk_paybill());
 			boolean isexists = true;
-			if(vo==null){
+			if (vo == null) {
 				isexists = false;
 			}
-			 String fpath = vo.getVfilepath();
-			 File afile = new File(fpath);
-			 if(!afile.exists()){
-				 isexists = false;
-			 }
-			 if(isexists){
-				 String path = getRequest().getSession().getServletContext().getRealPath("/");
-				 String typeiconpath = path + "images" + File.separator + "typeicon" + File.separator;
-				 os = getResponse().getOutputStream();
-				 is = new FileInputStream(afile);
-				 IOUtils.copy(is, os);
-			 }
+			String fpath = vo.getVfilepath();
+			File afile = new File(fpath);
+			if (!afile.exists()) {
+				isexists = false;
+			}
+			if (isexists) {
+				String path = getRequest().getSession().getServletContext().getRealPath("/");
+				String typeiconpath = path + "images" + File.separator + "typeicon" + File.separator;
+				os = getResponse().getOutputStream();
+				is = new FileInputStream(afile);
+				IOUtils.copy(is, os);
+			}
 		} catch (Exception e) {
-			
-		}finally{
-		    try {
-                if(os != null){
-                    os.close();
-                }
-                if(is!=null){
-                    is.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+
+		} finally {
+			try {
+				if (os != null) {
+					os.close();
+				}
+				if (is != null) {
+					is.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
-	
-	public void deleteImageFile(){
+
+	public void deleteImageFile() {
 		Json json = new Json();
 		try {
 			UserVO uservo = getLoginUserInfo();
 			pubser.checkFunnode(uservo, IFunNode.CHANNEL_36);
-			if(uservo != null && !"000001".equals(uservo.getPk_corp()) ){
+			if (uservo != null && !"000001".equals(uservo.getPk_corp())) {
 				throw new BusinessException("登陆用户错误");
-			}else if(uservo == null){
+			} else if (uservo == null) {
 				throw new BusinessException("登陆用户错误");
 			}
 			ChnPayBillVO paramvo = new ChnPayBillVO();
 			paramvo = (ChnPayBillVO) DzfTypeUtils.cast(getRequest(), paramvo);
-			if (StringUtil.isEmpty(paramvo.getPk_corp())){
+			if (StringUtil.isEmpty(paramvo.getPk_corp())) {
 				paramvo.setPk_corp(getLogincorppk());
 			}
-			if(!StringUtil.isEmpty(paramvo.getPk_paybill())){
+			if (!StringUtil.isEmpty(paramvo.getPk_paybill())) {
 				chnpay.deleteImageFile(paramvo);
 			}
 			json.setSuccess(true);
@@ -297,5 +308,5 @@ public class ChnPayBillAction extends BaseAction<ChnPayBillVO> {
 		}
 		writeJson(json);
 	}
-	
+
 }
