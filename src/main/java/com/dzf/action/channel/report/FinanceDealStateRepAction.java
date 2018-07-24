ba33fc4f231deba939eaea08c686cbc3a3985897
宗岩 <996290547@qq.com>
2018-07-24 20:19:27 +0800

    代码优化

--- src/main/java/com/dzf/action/channel/report/FinanceDealStateRepAction.java
+++ src/main/java/com/dzf/action/channel/report/FinanceDealStateRepAction.java
@@ -23,45 +23,49 @@ import com.dzf.service.pub.LogRecordEnum;
 
 /**
  * 财务处理情况反馈
+ * 
  * @author zy
  *
  */
 @ParentPackage("basePackage")
 @Namespace("/report")
 @Action(value = "financedealstaterep")
-public class FinanceDealStateRepAction extends BaseAction<FinanceDealStateRepVO>{
+public class FinanceDealStateRepAction extends BaseAction<FinanceDealStateRepVO> {
 
 	private static final long serialVersionUID = 995793827523865854L;
-	
+
 	private Logger log = Logger.getLogger(this.getClass());
-	
+
 	@Autowired
 	private IFinanceDealStateRep financeServ;
-	
+
 	/**
 	 * 查询
 	 */
-	public void query(){
+	public void query() {
 		Grid grid = new Grid();
 		try {
 			QryParamVO paramvo = new QryParamVO();
-			paramvo = (QryParamVO)DzfTypeUtils.cast(getRequest(), paramvo);
-			paramvo.setUser_name(getLoginUserid());
-			if(StringUtil.isEmpty(paramvo.getPk_corp())){
+			paramvo = (QryParamVO) DzfTypeUtils.cast(getRequest(), paramvo);
+			if(paramvo != null){
+				paramvo.setUser_name(getLoginUserid());
+			}
+			if (StringUtil.isEmpty(paramvo.getPk_corp())) {
 				paramvo.setPk_corp(getLogincorppk());
 			}
 			paramvo.setBegdate(new DZFDate());
 			List<FinanceDealStateRepVO> list = financeServ.query(paramvo);
 			int page = paramvo == null ? 1 : paramvo.getPage();
-			int rows = paramvo ==null ? 10000 : paramvo.getRows();
-		    int len = list == null ? 0 : list.size();
-		    if (len > 0) {
+			int rows = paramvo == null ? 10000 : paramvo.getRows();
+			int len = list == null ? 0 : list.size();
+			if (len > 0) {
 				grid.setTotal((long) (len));
-				grid.setRows(Arrays.asList(QueryUtil.getPagedVOs(list.toArray(new FinanceDealStateRepVO[0]), page, rows)));
+				grid.setRows(
+						Arrays.asList(QueryUtil.getPagedVOs(list.toArray(new FinanceDealStateRepVO[0]), page, rows)));
 				grid.setSuccess(true);
 				grid.setMsg("查询成功");
 				writeLogRecord(LogRecordEnum.OPE_CHANNEL_8.getValue(), "财务处理分析查询成功", ISysConstants.SYS_3);
-			}else{
+			} else {
 				grid.setTotal(Long.valueOf(0));
 				grid.setRows(list);
 				grid.setSuccess(true);
