58ead7f281facc76f78eb475899d9911109ae8c7
lwp <441958241@qq.com>
2018-08-09 11:51:39 +0800

    增加	类型

--- src/main/java/com/dzf/model/pub/IStatusConstant.java
+++ src/main/java/com/dzf/model/pub/IStatusConstant.java
@@ -34,11 +34,12 @@ public interface IStatusConstant {
 	public static int IPAYTYPE_2 = 2;
 	public static int IPAYTYPE_3 = 3;
 	
-	/** 合同明细单操作类型 (1:预付款付款；2：合同扣款；3：返点录入；4：退款单退款；) */
+	/** 合同明细单操作类型 (1:预付款付款；2：合同扣款；3：返点录入；4：退款单退款；5：商品购买；) */
 	public static int IDETAILTYPE_1 = 1;
 	public static int IDETAILTYPE_2 = 2;
 	public static int IDETAILTYPE_3 = 3;
 	public static int IDETAILTYPE_4 = 4;
+	public static int IDETAILTYPE_5 = 5;
 	
 	/** 首页查询类型(1:周查询；2：月查询；3：年查询；)*/
 	public static int IINDEXQRYTYPE_1 = 1;
@@ -79,4 +80,7 @@ public interface IStatusConstant {
 	/** 退款单操作(1：确认退款；1：取消确认；)*/
 	public static int IREFOPERATYPE_1 = 1;
 	public static int IREFOPERATYPE_2 = 2;
+	
+	/** 商品订单状态动作*/
+	public static String IGOODSACTION1 = "订单创建";
 }
