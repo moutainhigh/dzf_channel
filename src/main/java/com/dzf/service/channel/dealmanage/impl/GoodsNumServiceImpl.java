aa0ff9a6fba0a5b5730f706908efa30145ce4a53
宗岩 <996290547@qq.com>
2019-01-09 09:33:21 +0800

    商品数量明细表只展示入库数量大于0的商品；

--- src/main/java/com/dzf/service/channel/dealmanage/impl/GoodsNumServiceImpl.java
+++ src/main/java/com/dzf/service/channel/dealmanage/impl/GoodsNumServiceImpl.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.dzf.dao.bs.SingleObjectBO;
 import com.dzf.dao.jdbc.framework.SQLParameter;
 import com.dzf.dao.multbs.MultBodyObjectBO;
 import com.dzf.model.channel.stock.GoodsNumVO;
@@ -17,30 +16,28 @@ import com.dzf.service.channel.dealmanage.IGoodsNumService;
 
 @Service("numGoods")
 public class GoodsNumServiceImpl implements IGoodsNumService {
-	
-	@Autowired
-	private SingleObjectBO singleObjectBO;
-	
+
 	@Autowired
 	private MultBodyObjectBO multBodyObjectBO;
-	
-	
+
+	@SuppressWarnings("unchecked")
 	@Override
-	public List<GoodsNumVO> query(GoodsNumVO qvo) throws DZFWarpException{
-		QrySqlSpmVO sqpvo =  getQrySqlSpm(qvo);
-		List<GoodsNumVO> list = (List<GoodsNumVO>) multBodyObjectBO.queryDataPage(GoodsNumVO.class, 
-				sqpvo.getSql(), sqpvo.getSpm(), qvo.getPage(), qvo.getRows(), null);
+	public List<GoodsNumVO> query(GoodsNumVO qvo) throws DZFWarpException {
+		QrySqlSpmVO sqpvo = getQrySqlSpm(qvo);
+		List<GoodsNumVO> list = (List<GoodsNumVO>) multBodyObjectBO.queryDataPage(GoodsNumVO.class, sqpvo.getSql(),
+				sqpvo.getSpm(), qvo.getPage(), qvo.getRows(), null);
 		return list;
 	}
-	
+
 	@Override
 	public Integer queryTotalRow(GoodsNumVO qvo) throws DZFWarpException {
-		QrySqlSpmVO sqpvo =  getQrySqlSpm(qvo);
-		return multBodyObjectBO.queryDataTotal(StockOutVO.class,sqpvo.getSql(), sqpvo.getSpm());
+		QrySqlSpmVO sqpvo = getQrySqlSpm(qvo);
+		return multBodyObjectBO.queryDataTotal(StockOutVO.class, sqpvo.getSql(), sqpvo.getSpm());
 	}
-	
+
 	/**
 	 * 获取查询条件
+	 * 
 	 * @param pamvo
 	 * @return
 	 * @throws DZFWarpException
@@ -65,23 +62,23 @@ public class GoodsNumServiceImpl implements IGoodsNumService {
 		sql.append("    and nvl(num.dr, 0) = 0 ");
 		sql.append("    and nvl(t.dr, 0) = 0 ");
 		sql.append("    and nvl(s.dr, 0) = 0 ");
-		if(!StringUtil.isEmpty(qvo.getPk_goodstype())){
+		sql.append("    and num.istocknum > 0");//入库单数量大于0
+		if (!StringUtil.isEmpty(qvo.getPk_goodstype())) {
 			sql.append("and g.pk_goodstype=? ");
 			spm.addParam(qvo.getPk_goodstype());
 		}
-		if(!StringUtil.isEmpty(qvo.getVgoodscode())){
+		if (!StringUtil.isEmpty(qvo.getVgoodscode())) {
 			sql.append("and g.vgoodscode like ? ");
-			spm.addParam("%"+qvo.getVgoodscode()+"%");
+			spm.addParam("%" + qvo.getVgoodscode() + "%");
 		}
-		if(!StringUtil.isEmpty(qvo.getVgoodsname())){
+		if (!StringUtil.isEmpty(qvo.getVgoodsname())) {
 			sql.append("and g.vgoodsname like ? ");
-			spm.addParam("%"+qvo.getVgoodsname()+"%");
+			spm.addParam("%" + qvo.getVgoodsname() + "%");
 		}
 		sql.append(" order by t.vname,g.vgoodsname ");
 		qryvo.setSql(sql.toString());
 		qryvo.setSpm(spm);
 		return qryvo;
 	}
-	
 
 }
