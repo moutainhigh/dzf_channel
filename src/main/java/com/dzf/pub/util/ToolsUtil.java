d73d64ea7b17b3fae1997a7d40f9cbcda2b01cbe
宗岩 <996290547@qq.com>
2019-04-12 10:42:58 +0800

    添加方法

--- src/main/java/com/dzf/pub/util/ToolsUtil.java
+++ src/main/java/com/dzf/pub/util/ToolsUtil.java
@@ -309,4 +309,43 @@ public class ToolsUtil {
 		Integer preyear = year - nums;
 		return preyear + "-" + month;
 	}
+    
+    private static final int SQL_IN_LIST_LIMIT = 100;
+    
+    /**
+     * SQL拼接方法
+     * @param fieldname
+     * @param fieldvalue
+     * @return
+     */
+	public static String buildSqlForNotIn(final String fieldname,
+			final String[] fieldvalue) {
+		StringBuffer sbSQL = new StringBuffer();
+		sbSQL.append("(" + fieldname + " NOT IN ( ");
+		int len = fieldvalue.length;
+		// 循环写入条件
+		for (int i = 0; i < len; i++) {
+			if (fieldvalue[i] != null && fieldvalue[i].trim().length() > 0) {
+				sbSQL.append("'").append(fieldvalue[i].toString()).append("'");
+				// 单独处理 每个取值后面的",", 对于最后一个取值后面不能添加"," 并且兼容 oracle 的 IN 254 限制。每
+				// 200 个 数据 or 一次。时也不能添加","
+				if (i != (fieldvalue.length - 1)
+						&& !(i > 0 && (i + 1) % SQL_IN_LIST_LIMIT == 0)) {
+					sbSQL.append(",");
+				}
+			} else {
+				return null;
+			}
+
+			// 兼容 oracle 的 IN 254 限制。每 200 个 数据 or 一次。
+			if (i > 0
+					&& (i + 1) % SQL_IN_LIST_LIMIT == 0
+					&& i != (fieldvalue.length - 1)) {
+				sbSQL.append(" ) AND ").append(fieldname).append(" NOT IN ( ");
+			}
+		}
+		sbSQL.append(" )) ");
+
+		return sbSQL.toString();
+	}
 }
