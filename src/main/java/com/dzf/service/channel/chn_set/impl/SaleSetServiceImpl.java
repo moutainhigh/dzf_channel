4bf39aec43bb213035a0615b2f902f3a3feb3faa
gejw <gejingwei@dazhangfang.com>
2019-05-14 14:18:53 +0800

    gjw
    Signed-off-by: gejw <gejingwei@dazhangfang.com>

--- src/main/java/com/dzf/service/channel/chn_set/impl/SaleSetServiceImpl.java
+++ src/main/java/com/dzf/service/channel/chn_set/impl/SaleSetServiceImpl.java
@@ -11,16 +11,19 @@ import com.dzf.dao.jdbc.framework.processor.BeanListProcessor;
 import com.dzf.model.channel.sale.SaleSetVO;
 import com.dzf.model.sys.sys_power.UserVO;
 import com.dzf.pub.DZFWarpException;
+import com.dzf.pub.QueryDeCodeUtils;
 import com.dzf.pub.StringUtil;
-import com.dzf.pub.cache.UserCache;
 import com.dzf.service.channel.chn_set.ISaleSetService;
 import com.dzf.service.pub.LogRecordEnum;
+import com.dzf.service.sys.sys_power.IUserService;
 
 @Service("sale_set")
 public class SaleSetServiceImpl implements ISaleSetService{
 
 	@Autowired
 	private SingleObjectBO singleObjectBO = null;
+	@Autowired
+	private IUserService userServiceImpl = null;
 	
 	@Override
 	public SaleSetVO query(SaleSetVO vo) throws DZFWarpException {
@@ -35,7 +38,7 @@ public class SaleSetServiceImpl implements ISaleSetService{
 		SaleSetVO[] vos = (SaleSetVO[])singleObjectBO.queryByCondition(SaleSetVO.class, sql.toString(), sp);
 		if(vos!=null&&vos.length>0){
 			if(!StringUtil.isEmpty(vos[0].getLastmodifypsnid())){
-				UserVO uvo =  UserCache.getInstance().get(vos[0].getLastmodifypsnid(), null);
+				UserVO uvo =  userServiceImpl.queryUserJmVOByID(vos[0].getLastmodifypsnid());
 				vos[0].setLastmodifypsn(uvo.getUser_name());
 			}
 			return vos[0];
@@ -58,19 +61,15 @@ public class SaleSetServiceImpl implements ISaleSetService{
 	public List<SaleSetVO> queryHistory(SaleSetVO vo){
 		StringBuffer sql = new StringBuffer();
 		SQLParameter sp = new SQLParameter();//l.iopetype,l.vopemsg,
-		sql.append(" select l.doperatedate lastmodifydate,l.cuserid lastmodifypsnid from ynt_logrecord l ");
+		sql.append(" select l.doperatedate lastmodifydate,l.cuserid lastmodifypsnid,us.user_name as lastmodifypsn from ynt_logrecord l ");
+		sql.append(" left join sm_user us on us.cuserid = l.cuserid");
 		sql.append(" where l.vopemsg=? and l.pk_corp=? and nvl(l.dr,0)=0 order by l.ts desc");
 		sp.addParam(LogRecordEnum.OPE_CHANNEL_XSGZSZ.getName());
 		sp.addParam(vo.getPk_corp());
 		List<SaleSetVO> list = (List<SaleSetVO>) singleObjectBO.executeQuery(sql.toString(),sp,new BeanListProcessor(SaleSetVO.class));
 		UserVO uvo=null;
 		if(list!=null&&list.size()>0){
-			for (SaleSetVO saleSetVO : list) {
-				if(!StringUtil.isEmpty(saleSetVO.getLastmodifypsnid())){
-					uvo =  UserCache.getInstance().get(saleSetVO.getLastmodifypsnid(), null);
-					saleSetVO.setLastmodifypsn(uvo.getUser_name());
-				}
-			}
+		    QueryDeCodeUtils.decKeyUtils(new String[]{"lastmodifypsn"}, list, 1);
 			return list;
 		}else{
 			return null;
