3ddcdab3625f7037a5993fc462289cd6ab7e96e1
宗岩 <996290547@qq.com>
2019-08-01 11:58:33 +0800

    添加常量类

---
+++ src/main/java/com/dzf/pub/constant/IRoleCode.java
@@ -0,0 +1,31 @@
+package com.dzf.pub.constant;
+
+/**
+ * 在线业务角色编码
+ * 
+ * @author zy
+ *
+ */
+public interface IRoleCode {
+
+	/**
+	 * 渠道经理
+	 */
+	public static final String CORPQDJL = "corpqdjl";
+	
+	/**
+	 * 大区总
+	 */
+	public static final String CORPDQZ = "corpdqz";
+	
+	/**
+	 * 培训师
+	 */
+	public static final String CORPPXS = "corppxs";
+	
+	/**
+	 * 运营培训经理
+	 */
+	public static final String CORPPXJL = "corppxjl";
+	
+}
