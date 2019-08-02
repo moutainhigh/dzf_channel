dd6606710f80afaa8707b7d570c7b1b9bafd072f
宗岩 <996290547@qq.com>
2019-08-02 16:20:19 +0800

    代码格式化，添加注释

--- Embedded/webapps/dzf_channel/js/channel/report/repcommon.js
+++ Embedded/webapps/dzf_channel/js/channel/report/repcommon.js
@@ -1,19 +1,33 @@
-function initQryCommbox(){
+function initQryCommbox() {
 	changeArea();
 	changeProvince();
-	initArea({"qtype" :2});
-	initProvince({"qtype" :2});
-	initManager({"qtype" :2});
+	initArea({
+		"qtype" : 2
+	});
+	initProvince({
+		"qtype" : 2
+	});
+	initManager({
+		"qtype" : 2
+	});
 }
 
-function changeArea(){
-	 $("#aname").combobox({
+/**
+ * 大区改变事件
+ */
+function changeArea() {
+	$("#aname").combobox({
 		onChange : function(n, o) {
-			var queryData={"qtype" :2};
-			if(!isEmpty(n)){
-				queryData={'aname' : n,"qtype" :2};
-				$('#ovince').combobox('setValue',null);
-				$('#uid').combobox('setValue',null);
+			var queryData = {
+				"qtype" : 2
+			};
+			if (!isEmpty(n)) {
+				queryData = {
+					'aname' : n,
+					"qtype" : 2
+				};
+				$('#ovince').combobox('setValue', null);
+				$('#uid').combobox('setValue', null);
 			}
 			initProvince(queryData);
 			initManager(queryData);
@@ -21,34 +35,51 @@ function changeArea(){
 	});
 }
 
-//查询框关闭事件
+/**
+ * 查询框关闭事件
+ */
 function closeCx() {
 	$("#qrydialog").css("visibility", "hidden");
 }
 
-//清空查询条件
-function clearCondition(){
-	$('#aname').combobox('select',null);
-	$('#ovince').combobox('select',null);
-	$('#uid').combobox('select',null);
+/**
+ * 清空查询条件
+ */
+function clearCondition() {
+	$('#aname').combobox('select', null);
+	$('#ovince').combobox('select', null);
+	$('#uid').combobox('select', null);
 	$("#pk_account").val(null);
-	$("#channel_select").textbox("setValue",null);
+	$("#channel_select").textbox("setValue", null);
 }
 
-function changeProvince(){
-	 $("#ovince").combobox({
+/**
+ * 省(市)改变事件
+ */
+function changeProvince() {
+	$("#ovince").combobox({
 		onChange : function(n, o) {
-			var queryData={"qtype" :2};
-			if(!isEmpty(n)){
-				queryData={'aname' : $("#aname").combobox('getValue'),'ovince':n,"qtype" :2};
-				$('#uid').combobox('setValue',null);
+			var queryData = {
+				"qtype" : 2
+			};
+			if (!isEmpty(n)) {
+				queryData = {
+					'aname' : $("#aname").combobox('getValue'),
+					'ovince' : n,
+					"qtype" : 2
+				};
+				$('#uid').combobox('setValue', null);
 			}
 			initManager(queryData);
 		}
 	});
 }
 
-function initArea(queryData){
+/**
+ * 大区下拉初始化
+ * @param queryData
+ */
+function initArea(queryData) {
 	$.ajax({
 		type : 'POST',
 		async : false,
@@ -58,15 +89,22 @@ function initArea(queryData){
 		success : function(result) {
 			var result = eval('(' + result + ')');
 			if (result.success) {
-			    $('#aname').combobox('loadData',result.rows);
+				$('#aname').combobox('loadData', result.rows);
 			} else {
-				Public.tips({content : result.msg,type : 2});
+				Public.tips({
+					content : result.msg,
+					type : 2
+				});
 			}
 		}
 	});
 }
 
-function initProvince(queryData){
+/**
+ * 省(市)下拉初始化
+ * @param queryData
+ */
+function initProvince(queryData) {
 	$.ajax({
 		type : 'POST',
 		async : false,
@@ -76,15 +114,22 @@ function initProvince(queryData){
 		success : function(result) {
 			var result = eval('(' + result + ')');
 			if (result.success) {
-			    $('#ovince').combobox('loadData',result.rows);
+				$('#ovince').combobox('loadData', result.rows);
 			} else {
-				Public.tips({content : result.msg,type : 2});
+				Public.tips({
+					content : result.msg,
+					type : 2
+				});
 			}
 		}
 	});
 }
 
-function initManager(queryData){
+/**
+ * 会计运营经理初始化
+ * @param queryData
+ */
+function initManager(queryData) {
 	$.ajax({
 		type : 'POST',
 		async : false,
@@ -94,61 +139,75 @@ function initManager(queryData){
 		success : function(result) {
 			var result = eval('(' + result + ')');
 			if (result.success) {
-			    $('#uid').combobox('loadData',result.rows);
+				$('#uid').combobox('loadData', result.rows);
 			} else {
-				Public.tips({content : result.msg,type : 2});
+				Public.tips({
+					content : result.msg,
+					type : 2
+				});
 			}
 		}
 	});
 }
 
-//初始化加盟商
-function initChannel(){
-    $('#channel_select').textbox({
-        editable: false,
-        icons: [{
-            iconCls: 'icon-search',
-            handler: function(e) {
-                $("#kj_dialog").dialog({
-                    width: 600,
-                    height: 480,
-                    readonly: true,
-                    title: '选择加盟商',
-                    modal: true,
-                    href: DZF.contextPath + '/ref/channel_select.jsp',
-                    queryParams : {
-    					ovince :"-5"
-    				},
-                    buttons: '#kj_buttons'
-                });
-            }
-        }]
-    });
+/**
+ * 加盟商初始化
+ */
+function initChannel() {
+	$('#channel_select').textbox({
+		editable : false,
+		icons : [ {
+			iconCls : 'icon-search',
+			handler : function(e) {
+				$("#kj_dialog").dialog({
+					width : 600,
+					height : 480,
+					readonly : true,
+					title : '选择加盟商',
+					modal : true,
+					href : DZF.contextPath + '/ref/channel_select.jsp',
+					queryParams : {
+						ovince : "-5"
+					},
+					buttons : '#kj_buttons'
+				});
+			}
+		} ]
+	});
 }
 
-function selectCorps(){
+/**
+ * 双击选择加盟商
+ */
+function selectCorps() {
 	var rows = $('#gsTable').datagrid('getSelections');
 	dClickCompany(rows);
 }
 
-//双击选择公司
-function dClickCompany(rowTable){
+/**
+ * 双击选择加盟商
+ * @param rowTable
+ */
+function dClickCompany(rowTable) {
 	var str = "";
 	var corpIds = [];
-	if(rowTable){
-		if(rowTable.length>300){
-			Public.tips({content : "一次最多只能选择300个客户!" ,type:2});
+	if (rowTable) {
+		if (rowTable.length > 300) {
+			Public.tips({
+				content : "一次最多只能选择300个客户!",
+				type : 2
+			});
 			return;
 		}
-		for(var i=0;i<rowTable.length;i++){
-			if(i == rowTable.length - 1){
+		for (var i = 0; i < rowTable.length; i++) {
+			if (i == rowTable.length - 1) {
 				str += rowTable[i].uname;
-			}else{
-				str += rowTable[i].uname+",";
+			} else {
+				str += rowTable[i].uname + ",";
 			}
 			corpIds.push(rowTable[i].pk_gs);
 		}
-		$("#channel_select").textbox("setValue",str);
+		$("#channel_select").textbox("setValue", str);
 		$("#pk_account").val(corpIds);
 	}
 	$("#kj_dialog").dialog('close');
